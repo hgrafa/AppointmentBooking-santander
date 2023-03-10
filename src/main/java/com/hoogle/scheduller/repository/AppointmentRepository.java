d8ccf02e9f6b7d03e7db66ec1f439f7bbdbab8cc
hgrafa <hugorafell@gmail.com>
2023-03-09 23:48:24 -0300

    chore: adding repository

---
+++ src/main/java/com/hoogle/scheduller/repository/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package com.hoogle.scheduller.repository;
+
+import com.hoogle.scheduller.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    // especificacao
+}
