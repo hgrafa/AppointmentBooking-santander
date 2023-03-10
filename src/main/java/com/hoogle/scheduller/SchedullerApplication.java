93547c5523ec971b83635cf38d6536f4ccf579c0
hgrafa <hugorafell@gmail.com>
2023-03-09 23:50:07 -0300

    í¹chore: application update

---
+++ src/main/java/com/hoogle/scheduller/SchedullerApplication.java
@@ -0,0 +1,13 @@
+package com.hoogle.scheduller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedullerApplication.class, args);
+    }
+
+}
