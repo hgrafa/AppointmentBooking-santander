877ceac40dbddfe44f21328ae91f3d5ad332e383
hgrafa <hugorafell@gmail.com>
2023-03-09 22:03:54 -0300

    chore: files...

---
+++ src/test/java/com/hoogle/scheduller/SchedullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoogle.scheduller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedullerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
